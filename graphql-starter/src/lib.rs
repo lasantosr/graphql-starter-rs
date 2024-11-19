044bd38710f5136e56bc63b61d885e84c4f65d16
Luis Alberto Santos <lasantosr.92@gmail.com>
2024-11-19 22:10:06 +0100

    re-export error types

--- graphql-starter/src/lib.rs
+++ graphql-starter/src/lib.rs
@@ -9,6 +9,10 @@ pub mod request_id;
 pub mod serde;
 pub mod timeout;
 
+pub use error::{ApiError, Error, Result};
+#[cfg(feature = "graphql")]
+pub use error::{GraphQLError, GraphQLResult};
+
 #[cfg(feature = "ansi")]
 pub mod ansi;
 
